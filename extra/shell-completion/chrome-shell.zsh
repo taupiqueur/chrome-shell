b4af782f829bd50480de91a18aef2f21b1c8c8ae
Mathieu Ablasou <taupiqueur.kanto@gmail.com>
2023-09-18 21:12:16 +0200

    Add Zsh completion

---
+++ extra/shell-completion/chrome-shell.zsh
@@ -0,0 +1,35 @@
+#compdef chrome-shell
+
+local OPTION_WORDLIST=(
+  "--target=[Specifies the target platform]:platform:_chrome_shell_platforms"
+  "*-v[Increases the level of verbosity (the max level is -vvv)]"
+  "--log=[Specifies the file to use for logging]:file:_files"
+  "-h[Show this help]"
+  "--help[Show this help]"
+  "-V[Show version]"
+  "--version[Show version]"
+)
+
+local COMMAND_WORDLIST=(
+  "run[Run server]"
+  "install[Install manifest and register specified extensions]"
+  "uninstall[Uninstall manifest]"
+)
+
+local PLATFORM_WORDLIST=(
+  "chrome[Chrome]"
+  "chrome-dev[Chrome Dev]"
+  "chrome-beta[Chrome Beta]"
+  "chrome-canary[Chrome Canary]"
+  "chromium[Chromium]"
+)
+
+_chrome_shell_commands() {
+  _values "command" "${COMMAND_WORDLIST[@]}"
+}
+
+_chrome_shell_platforms() {
+  _values "platform" "${PLATFORM_WORDLIST[@]}"
+}
+
+_arguments -S -s "${OPTION_WORDLIST[@]}" "1:command:_chrome_shell_commands" "*:file:_files"
