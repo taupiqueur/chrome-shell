3eee72183f655fc0d063bb631b755df91fed4f21
Mathieu Ablasou <taupiqueur.kanto@gmail.com>
2024-07-01 12:55:12 +0200

    Add Windows support

--- src/installer.cr
+++ src/installer.cr
@@ -19,6 +19,7 @@ class Installer
     if File.exists?(manifest_path)
       merge_manifest(manifest_path)
     end
+    Dir.mkdir_p(manifest_path.parent)
     File.open(manifest_path, "w") do |file|
       manifest.to_json(file)
     end
@@ -26,6 +27,10 @@ class Installer
       "Manifest installed",
       manifest_path: manifest_path.to_s
     )
+
+    {% if flag?(:windows) %}
+      reg_add(reg_key, manifest_path.to_s)
+    {% end %}
   end
 
   def merge_manifest(manifest_path : Path = manifest_path)
@@ -41,5 +46,59 @@ class Installer
       "Manifest uninstalled",
       manifest_path: manifest_path.to_s
     )
+
+    {% if flag?(:windows) %}
+      reg_delete(reg_key)
+    {% end %}
+  end
+
+  # Adds a new entry to the Windows registry.
+  # Reference: https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/reg-add
+  private def reg_add(key : String, value : String)
+    command_status = Process.run(
+      "REG",
+      {"ADD", key, "/ve", "/t", "REG_SZ", "/d", value, "/f"}
+    )
+    unless command_status.success?
+      raise RuntimeError.new
+    end
+    Log.info &.emit(
+      "Registry entry added",
+      key: key,
+      value: value
+    )
+  end
+
+  # Deletes specified entry from the Windows registry.
+  # Reference: https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/reg-delete
+  private def reg_delete(key : String)
+    command_status = Process.run(
+      "REG",
+      {"DELETE", key, "/f"}
+    )
+    unless command_status.success?
+      raise RuntimeError.new
+    end
+    Log.info &.emit(
+      "Registry entry deleted",
+      key: key
+    )
+  end
+
+  # Returns the native app’s registry key.
+  # Reference: https://developer.chrome.com/docs/extensions/mv3/nativeMessaging/#native-messaging-host-location
+  private def reg_key : String
+    case platform
+    in .chrome?
+      "HKCU\\Software\\Google\\Chrome\\NativeMessagingHosts\\#{manifest.name}"
+    in .chrome_dev?
+      "HKCU\\Software\\Google\\Chrome Dev\\NativeMessagingHosts\\#{manifest.name}"
+    in .chrome_beta?
+      "HKCU\\Software\\Google\\Chrome Beta\\NativeMessagingHosts\\#{manifest.name}"
+    in .chrome_canary?
+      "HKCU\\Software\\Google\\Chrome Canary\\NativeMessagingHosts\\#{manifest.name}"
+    in .chromium?
+      "HKCU\\Software\\Chromium\\NativeMessagingHosts\\#{manifest.name}"
+    end
   end
 end
